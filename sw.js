91af7a1935ba962c8684effaa1c4d2272c0e3a80
nantoka-el <nakayama@webly.jp>
2025-06-25 07:09:17 +0900

    Add PWA support with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'bedburn-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sounds/start.mp3',
+  './sounds/tick.mp3',
+  './sounds/done.mp3',
+  'https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.4/howler.min.js',
+  'https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.1/dist/confetti.browser.min.js'
+];
+
+// install
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+// fetch
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
