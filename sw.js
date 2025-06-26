355c07b5328fccc59484e1294e276b1b442d2087
nantoka-el <fanky.kosuke@me.com>
2025-06-27 03:58:45 +0900

    Add next phase display and praise sound

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ const FILES = [
   './sounds/start.mp3',
   './sounds/tick.mp3',
   './sounds/done.mp3',
+  './sounds/praise.mp3',
   'https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.4/howler.min.js',
   'https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.1/dist/confetti.browser.min.js'
 ];
